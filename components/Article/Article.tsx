f2da70e76dd5684fd0010881308765476bf642e8
Andrew Barrows <abarrows@amuniversal.com>
2024-03-24 15:56:42 -0500

    [BC-01] Delete unused article and page components, and add new Table component

--- components/Article/Article.tsx
+++ components/Article/Article.tsx
@@ -1,5 +1,32 @@
 import React from 'react';
 
-export default function Article({ children }) {
-  return <article>{children}</article>;
+import { database } from '@/utilities/prisma';
+
+export default async function Article({ id: number }) {
+  // Retrieve the article from the database using the id
+  const article = await database.trainingItem.findUnique({
+    where: {
+      id: number,
+    },
+  });
+
+  return (
+    <main>
+      <article className='prose dark:prose-dark max-w-none'>
+        <h1>{article?.title}</h1>
+        <section>{article?.body}</section>
+        <aside>
+          <p>{article?.url}</p>
+          <p>{article?.key}</p>
+          <p>{article?.updatedAt.toDateString()}</p>
+          <p>{article?.createdAt.toDateString()}</p>
+          <p>{article?.labels}</p>
+          <p>{article?.proficiencies}</p>
+          <p>{article?.tools}</p>
+          <p>{article?.advancedSkills}</p>
+        </aside>
+        {article.body}
+      </article>
+    </main>
+  );
 }
